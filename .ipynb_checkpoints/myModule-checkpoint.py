6a79021d56b56ce12f8de6e6f059d287eb1c541b
gpalakgupta <palakgupta5709@gmail.com>
2025-07-04 14:09:57 +0530

    Add jupyter notebooks

---
+++ .ipynb_checkpoints/myModule-checkpoint.py
@@ -0,0 +1,72 @@
+print("Hello !! Welcome to my module")
+a = 10
+b = "Hello"
+
+class A:
+    pass
+
+def fn():
+    a,b = 10,20
+    c = a+b
+    return c
+print(__name__)
+if __name__ == '__main__':
+    print(fn())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
